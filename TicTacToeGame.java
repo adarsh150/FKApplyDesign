5ca7cde4b5b8ec6a44bcf87b87ee70a776a1966b
adarsh150 <agrawal.adarsh3004@gmail.com>
2020-01-25 11:49:11 +0530

    Feature2 -> Method printBoard() 		-->which will print the current state of Board. 	   Added printBoard() to both class (in Player1 and Player2)

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -15,6 +15,19 @@ interface Board{
 
 
 class Player1 implements Board{
+
+	public void printBoard()
+	{
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+	
 	public boolean isBoardFull()
 	{
 		for(int i=0;i<3;i++)
@@ -30,6 +43,21 @@ class Player1 implements Board{
 }
 
 class Player2 implements Board{
+
+
+	public void printBoard()
+	{
+		for(int i=0;i<3;i++)
+		{
+			for(int j=0;j<3;j++)
+			{
+				System.out.print(board[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+
+
 	public boolean isBoardFull()
 	{
 		for(int i=0;i<3;i++)
